995113b93c6e2229b6ccd8db1ad0e854951132ea
luix silva <felipeluiz75@outlook.com>
2024-08-02 07:34:01 -0300

    Add more components

--- src/components/Grid/index.jsx
+++ src/components/Grid/index.jsx
@@ -1,1 +1,11 @@
 import React from "react";
+import "../Grid/index.css";
+
+export const Grid = ({ children, display = "block" }) => {
+  return <div className={`grid-container ${display}`}>{children}</div>;
+};
+
+export const GridItem = ({ children, xs, md, lg }) => {
+  const classes = `grid-item grid-item-xs-${xs} grid-item-md-${md} grid-item-lg-${lg}`;
+  return <div className={classes}>{children}</div>;
+};
--- src/pages/public/login/login.jsx
+++ src/pages/public/login/login.jsx
@@ -1,24 +1,34 @@
 import React from "react";
+import "../login/login.css"
 import { Field } from "../../../components/Fields/TextFieldDefault";
+import { Grid, GridItem } from "../../../components/Grid";
 
 export const Login = () => {
   return (
     <>
-      <div>
-        <Field
-          label="Usuário"
-          type="text"
-          name="user"
-          id="nome"
-          placeholder="Digite seu usuário"
-        />
-        <Field
-          label="Senha"
-          type="password"
-          name="password"
-          id="password"
-          placeholder="Digite sua senha"
-        />
+      <div className="login-container">
+        <div className="login-grid">
+          <Grid display="flex">
+            <GridItem xs={12} md={4} lg={4}>
+              <Field
+                label="Usuário"
+                type="text"
+                name="user"
+                id="nome"
+                placeholder="Digite seu usuário"
+              />
+            </GridItem>
+            <GridItem xs={12} md={4} lg={4}>
+              <Field
+                label="Senha"
+                type="password"
+                name="password"
+                id="password"
+                placeholder="Digite sua senha"
+              />
+            </GridItem>
+          </Grid>
+        </div>
       </div>
     </>
   );
